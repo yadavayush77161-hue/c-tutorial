0a6cf94b822f47e654b7c7e96eae4e096fa325b4
yadavayush77161-hue <yadavayush77161@gmail.com>
2026-09-24 09:41:57 +0530

    Add new code

---
+++ 26.cpp
@@ -0,0 +1,33 @@
+//Wap prong to use bubble short alsorithm and sort array elements 
+#include<iostream>
+using namespace std;
+
+void bubblesort(int arr[],int n)
+{
+    for(int i = 0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                 swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+void printArray(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    int arr[] = {4,1,5,2,3,10};
+    int n = 6;
+    bubblesort(arr,n);
+    printArray(arr,n);
+    return 0;
+}
